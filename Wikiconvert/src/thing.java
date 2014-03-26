2a49d1ded9f8f8abdc15d8e372e68f270b20f987
tc_yog <tc_yog@10.14.10.155>
2014-03-26 16:06:09 -0700

    first draft

---
+++ Wikiconvert/src/thing.java
@@ -0,0 +1,105 @@
+  import java.io.*;     
+import java.util.*;     
+
+public class thing {
+
+public static void main(String [] args){     
+	try {
+		ArrayList<String> lines = new ArrayList<String>();
+
+
+    	    BufferedReader in = new BufferedReader(new FileReader(args[0]));
+    	    String line = in.readLine();{
+    	    	int strike = 0;
+    	    	int comment = 0;
+    	    	int noedit = 0;
+    	    	while (line != null) {
+    	    		if (line.contains("{{{#!")) {
+         	            line = line.replace("{{{", "<!--");
+         	            comment = 1;
+        	    	 }
+    	    		if (line.contains("{{{")) {
+        	            line = line.replace("{{{", "<pre>");
+        	            noedit = 1;
+    	    		}
+    	    		if (line.contains("}}}")) {
+        	        	if (comment == 0){
+        	            line = line.replace("}}}", "</pre>");
+        	            noedit = 0;
+        	        	}
+        	            else{
+        	            line = line.replace("}}}", "-->");
+        	            comment = 0;
+        	            }	
+        	            }
+    	    	if (noedit == 1){
+    	    	}
+    	    	else{
+    	    	if (line.contains("**")) {
+    	            line = line.replace("**", "'''");
+    	        }
+    	    	if (line.contains("--")) {
+    	        	if (strike == 0){
+    	            line = line.replace("--", "<strike>");
+    	        	strike += 1;}
+    	            else if (strike == 1){
+    	            line = line.replace("--", "</strike>");
+    	            strike -= 1;
+    	            }	
+    	            }
+    	    	 
+    	    	if (line.startsWith(" ")){
+    	        	
+    	        	if (line.startsWith("*", 1)){
+    	        		line = line.replace(" *", "**");
+    	        	  	}
+    	        	else if (line.startsWith(" *", 1)){
+    	        		line = line.replace("  *", "***");
+    	        		}
+    	        	else if (line.startsWith("  *", 1)){
+    	        		line = line.replace("   *", "****");
+    	        		}
+    	        	else{
+    	        		line = "<blockquote>" + line + "</blockquote>";
+    	        	}
+    	        	
+    	    	}
+    	        if (line.startsWith("=")) {
+	        		line = "=" + line + "=";
+	        			
+	        	}
+    	    	}
+    	        lines.add(line);
+    	        line = in.readLine();
+    	    }
+    	   
+
+    	    
+    	    in.close();
+ 
+		    PrintWriter out = new PrintWriter(args[0] + ".new.txt");
+		    for (String l : lines)
+		        out.println(l + '\n');
+		    out.close();
+			
+
+			
+			System.out.println("Done");
+    	    }
+    	    
+    	}catch (IOException e)
+    	{
+    		e.printStackTrace();}
+    
+    	//try {
+
+ 
+		//} catch (IOException e) {
+			//e.printStackTrace();
+		//}
+
+    	
+          
+                 
+            } 
+    }
